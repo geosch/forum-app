edea8f7d1fbc3ed3989dd8c9034f98b772555863
Roland <urbano.roland@google.com>
2013-04-24 14:53:34 +0200

    Test Suite Project

---
+++ forumAppTestSuite/src/com/example/forum_app/test/AllTests.java
@@ -0,0 +1,16 @@
+package com.example.forum_app.test;
+
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import android.test.suitebuilder.TestSuiteBuilder;
+
+
+/**
+ * A test suite containing all tests for my application.
+ */
+public class AllTests extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(AllTests.class).includeAllPackagesUnderHere().build();
+    }
+}
---
+++ forumAppTestSuite/src/com/example/forum_app/test/SomeTest.java
@@ -0,0 +1,17 @@
+package com.example.forum_app.test;
+
+
+import junit.framework.Assert;
+import android.test.AndroidTestCase;
+
+
+public class SomeTest extends AndroidTestCase {
+
+    public void testSomething() throws Throwable {
+       Assert.assertTrue((1 + 1) == 2);
+    }
+
+    public void testSomethingElse() throws Throwable {
+       Assert.assertTrue(1 + 1 == 2);
+    }
+}
