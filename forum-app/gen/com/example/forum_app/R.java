4c4d1bab5ec70f9ea78ead868e3d5e9c33b48739
polat <polat@polat-Studio-XPS-1340.(none)>
2013-05-15 14:42:23 +0200

    error message in middle

--- forum-app/gen/com/example/forum_app/R.java
+++ forum-app/gen/com/example/forum_app/R.java
@@ -9,9 +9,13 @@ package com.example.forum_app;
 
 public final class R {
     public static final class array {
-        public static final int expandableListView1=0x7f060000;
+        public static final int expandableListView1=0x7f060001;
+        public static final int gender=0x7f060000;
     }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int red=0x7f070000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -24,31 +28,92 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int catroid=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090006;
-        public static final int expandable_list=0x7f090001;
-        public static final int listView1=0x7f090000;
-        public static final int list_item=0x7f090004;
-        public static final int list_item_child=0x7f090002;
-        public static final int list_item_text_child=0x7f090003;
-        public static final int list_item_text_view=0x7f090005;
+        public static final int action_forgot_password=0x7f0a0017;
+        public static final int action_settings=0x7f0a0018;
+        public static final int btRegister=0x7f0a000f;
+        public static final int email=0x7f0a0003;
+        public static final int etEmail=0x7f0a000e;
+        public static final int etNickname=0x7f0a000a;
+        public static final int etPassword=0x7f0a000b;
+        public static final int etPasswordConfirm=0x7f0a000c;
+        public static final int expandable_list=0x7f0a0008;
+        public static final int listView1=0x7f0a0007;
+        public static final int list_item=0x7f0a0014;
+        public static final int list_item_child=0x7f0a0012;
+        public static final int list_item_text_child=0x7f0a0013;
+        public static final int list_item_text_view=0x7f0a0015;
+        public static final int list_view=0x7f0a0016;
+        public static final int login=0x7f0a0005;
+        public static final int login_form=0x7f0a0002;
+        public static final int login_status=0x7f0a0000;
+        public static final int login_status_message=0x7f0a0001;
+        public static final int password=0x7f0a0004;
+        public static final int register=0x7f0a0009;
+        public static final int sign_in_button=0x7f0a0006;
+        public static final int spCountry=0x7f0a0010;
+        public static final int spGender=0x7f0a0011;
+        public static final int tvRegisterError=0x7f0a000d;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int list_item_child=0x7f030001;
-        public static final int list_item_parent=0x7f030002;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_register=0x7f030002;
+        public static final int list_item_child=0x7f030003;
+        public static final int list_item_parent=0x7f030004;
+        public static final int list_view=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int login=0x7f090000;
+        public static final int main=0x7f090001;
+        public static final int register=0x7f090002;
     }
     public static final class string {
+        public static final int action_forgot_password=0x7f050022;
         public static final int action_settings=0x7f050001;
+        public static final int action_sign_in_register=0x7f050020;
+        public static final int action_sign_in_short=0x7f050021;
         public static final int app_name=0x7f050000;
-        public static final int categories=0x7f050004;
+        public static final int categories=0x7f05001a;
+        public static final int country_language=0x7f050008;
+        public static final int country_language_state=0x7f050009;
+        public static final int country_prompt=0x7f050007;
+        public static final int email=0x7f050005;
+        public static final int err_invalid_email=0x7f050015;
+        public static final int err_missing_country=0x7f050011;
+        public static final int err_missing_email=0x7f050012;
+        public static final int err_missing_gender=0x7f050010;
+        public static final int err_missing_nickname=0x7f05000d;
+        public static final int err_missing_password=0x7f05000e;
+        public static final int err_missing_passwordconfirm=0x7f05000f;
+        public static final int err_too_short_passwords=0x7f050013;
+        public static final int err_unequal_passwords=0x7f050014;
+        public static final int error_field_required=0x7f050027;
+        public static final int error_incorrect_password=0x7f050026;
+        public static final int error_invalid_email=0x7f050024;
+        public static final int error_invalid_password=0x7f050025;
+        public static final int gender_prompt=0x7f050006;
         public static final int hello_world=0x7f050002;
-        public static final int new_posts=0x7f050003;
+        public static final int login=0x7f05001c;
+        public static final int login_progress_signing_in=0x7f050023;
+        public static final int new_posts=0x7f050019;
+        public static final int nickname=0x7f050004;
+        public static final int ok=0x7f050018;
+        public static final int password=0x7f05000a;
+        public static final int passwordconfirm=0x7f05000b;
+        /**  Strings related to login 
+         */
+        public static final int prompt_email=0x7f05001e;
+        public static final int prompt_password=0x7f05001f;
+        public static final int register=0x7f05000c;
+        public static final int registration_complete_message=0x7f050017;
+        public static final int registration_complete_title=0x7f050016;
+        public static final int title_activity_login=0x7f05001d;
+        public static final int title_activity_register=0x7f050003;
+        public static final int top_bar=0x7f05001b;
     }
     public static final class style {
         /** 
@@ -72,10 +137,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
+        public static final int LoginFormContainer=0x7f080002;
     }
 }
